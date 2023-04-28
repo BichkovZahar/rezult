8344a1c3088c1466dc84e45ca677cd1995857605
Asus <Bichk_di78@student.itstep.org>
2023-04-28 11:11:23 +0300

    2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def lesson_1(*args):
-    suma = sum(args)
-    room = len(args)
-    print("Середньо арифметичне:" , suma / room)
-lesson_1(5 , 7 , 19 , 4)
+def lesson_2(*args):
+    count = 0
+    for finish in args:
+        if len(finish) > count:
+            count = len(finish)
+    return print('Максимальне кількість символів в рядку:' , count)
+lesson_2("Red" , "Finish" , "Value" , "Start")
